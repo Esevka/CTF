92079e43ddbfbc167ec8dbda5ae02612f76a4653
EsevKa <139042999+Esevka@users.noreply.github.com>
2025-02-04 12:10:22 +0100

    Update steghide_brute.py

--- Scripts en Python/steghide_brute.py
+++ Scripts en Python/steghide_brute.py
@@ -1,18 +1,22 @@
 import subprocess
 
-def brute(line):
-	result = subprocess.run(['steghide extract -sf **IMG** -p {}'.format(line)],shell=True,capture_output=True,text=True)
+def brute(line,x):
+	result = subprocess.run(['steghide extract -sf **IMG** -f -p {}'.format(line)],shell=True,capture_output=True,text=True)
 
 	if(result.returncode==1):
-		print('[-]Clave Incorrecta --> {}'.format(line))
+		print('[{}-5000]Clave Incorrecta --> {}'.format(x,line))
 	else:
 		print('[+]Clave --> {}'.format(line))
+		print('[+]INFO --> {}'.format(result.stderr))
 		exit()
 		
 def main():
+	x=0
+	
 	with open('**Diccionario**','r') as passwd:
 		for line in passwd:
-			brute(line.strip())
+			x+=1
+			brute(line.strip(),x)
 
 if __name__ == '__main__':
 	main()
